bb8da87ef2cb8af3bfdddb550d251f743fba7512
Diego Araujo <diegozigoto@gmail.com>
2021-05-03 17:32:04 -0300

    Rest com swagger pronta

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,7 +1,10 @@
 'use strict';
 var carRouter = require("./car/car.router.js");
+var swaggerUi = require('swagger-ui-express');
+var swaggerDocument = require('../swagger/swagger-definition.json');
 
 var register = function (app) {
+  app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use('/', carRouter);
 };
 
